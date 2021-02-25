1d47fc59f517e0306a48675df403799bccd81ba9
Steven Garzon <steven.garzon@mail.escuelaing.edu.co>
2021-02-25 08:06:50 -0500

    Subir el correcto 2

--- covidAnalyzerTool/src/main/java/eci/arsw/covidanalyzer/TestReporter.java
+++ covidAnalyzerTool/src/main/java/eci/arsw/covidanalyzer/TestReporter.java
@@ -17,7 +17,7 @@ public class TestReporter {
     public static final int TRUE_NEGATIVE = 2;
     public static final int FALSE_NEGATIVE = 3;
 
-    private static final String SERVER_URL = "http://localhost:8080/covid/result";
+    private static final String SERVER_URL = "http://covidanalyzer2021.herokuapp.com/covid/result";
 
     private static ObjectMapper mapper = new ObjectMapper();
 
